2f20f92917701a0a66c48ec698d3f697ea6e49bf
Felicia Heiney <bhfelicia@gmail.com>
2021-02-13 12:23:34 -0500

    creates dropdown form to select a person who can eventually make a purchase

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const {
 
 const app = express();
 
+app.use(require("method-override")("_method"));
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", async (req, res, next) => {
   try {
     const [people, places, things, purchases] = await Promise.all([
@@ -18,31 +21,41 @@ app.get("/", async (req, res, next) => {
         include: [Person, Place, Thing],
       }),
     ]);
-    // res.send(html`
-    //   <html>
-    //     <head>
-    //       <title>Purchases Abroad</title>
-    //     </head>
-    //     <body>
-    //       <h1>Acme Purchases Abroad</h1>
-    //       <form method="POST">
-    //         <select name="personId">
-    //           <option></option>
-    //           ${people.map((person) => {
-    //             html` <option value="${person.id}"></option> `;
-    //           })}
-    //         </select>
-    //       </form>
-    //       <div>
-    //         <h2>What They Bought</h2>
-    //         <ul>
-    //           ${purchases.map((purchase) => html` <li></li>`)}
-    //         </ul>
-    //       </div>
-    //     </body>
-    //   </html>
-    // `);
-      res.send({ people, places, things, purchases });
+    res.send(html`
+      <html>
+        <head>
+          <title>Purchases Abroad</title>
+        </head>
+        <body>
+          <h1>Acme Purchases Abroad</h1>
+          <h3>Add a purchase!</h3>
+          <form method="POST">
+            <select name="personId" action="/people/${people.id}">
+              <option>Person</option>
+                ${people.map(
+                  (person) =>
+                    html` <option value="${person.id}">${person.name}</option> `
+                )}
+              </option>
+            </select>
+
+            <button>Save Purchase</button>
+          </form>
+          <div>
+            <h2>What They Bought</h2>
+            <ul>
+              ${purchases.map(
+                (purchase) => html` <li>
+                  ${purchase.person.name} purchased ${purchase.quantity}
+                  ${purchase.thing.name} on ${purchase.date}
+                </li>`
+              )}
+            </ul>
+          </div>
+        </body>
+      </html>
+    `);
+    // res.send({ people, places, things, purchases });
   } catch (err) {
     next(err);
   }
